8a889f04c6ab316c72a75aeb2460a654e9aa299e
JayVillain <saipi3825@gmail.com>
2025-07-31 02:06:01 +0000

    Jul 31, 2025, 9:06 AM

--- dork.py
+++ dork.py
@@ -6,7 +6,7 @@ import base64 # Import modul base64
 # --- Konfigurasi ---
 # Ganti dengan email dan kunci API Fofa kamu
 FOFA_EMAIL = "@gmail.com"
-FOFA_KEY = ""
+FOFA_KEY = "api lu nih "
 
 # Nama file untuk menyimpan URL yang ditemukan
 OUTPUT_FILENAME = "fofa_urls.txt"
